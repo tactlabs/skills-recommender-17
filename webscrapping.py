5490b0152efd4bdb5c26729e5cc405b9a7510b0a
MOURYA SAI CHARAN REDDY KYPA <36075316+Mouryasai27@users.noreply.github.com>
2018-11-27 20:49:54 +0530

    Add files via upload

---
+++ webscrapping.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+
+user_agent = UserAgent()
+for page in range(1,39):
+    main_url = 'https://stackoverflow.com/jobs?med=site-ui&ref=jobs-tab'+str(page)
+    page = requests.get(main_url,headers={'user-agent':user_agent.chrome})
+    soup = BeautifulSoup(page.content,'lxml')
+
+    job_sum = soup.find_all("div", {"class": "js-search-results flush-left"})
+
+    for job in job_sum:
+        job_title = job.find_all("a", {"class": "s-link s-link__visited"})
+        job_skill = job.find_all("div", {"class": "mt12 -tags"})
+
+    for i in range(len(job_skill)):
+        print(job_title[i].string, end="-------------------")
+        skills = job_skill[i].find_all("a", {"class": "post-tag job-link no-tag-menu"})
+        for skill in skills:
+            print(skill.string, end=",")
+        print("\n")
